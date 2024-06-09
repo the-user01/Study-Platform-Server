a18e074b560517d37212a3b585e0a2150d3d4e66
the-user01 <sakibcom02@gmail.com>
2024-06-09 16:55:18 +0600

    User delete function added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const port = process.env.PORT || 5000;
 
@@ -35,6 +35,13 @@ async function run() {
     const usersCollection = client.db("studyPlatformDb").collection("users");
 
     // users related api
+
+    app.get('/users', async(req, res)=>{
+      const result = await usersCollection.find().toArray();
+      res.send(result)
+    })
+
+
     app.post("/users", async(req, res)=>{
       const user = req.body;
       // insert email if user doesn't exist
@@ -46,6 +53,13 @@ async function run() {
 
       const result = await usersCollection.insertOne(user);
       res.send(result)
+    })
+
+    app.delete('/users/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await usersCollection.deleteOne(query);
+      res.send(result);
     })
 
 
